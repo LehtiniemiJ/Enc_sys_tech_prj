056c7300938b7870ae89c61fce8696f5675b6902
LehtiniemiJ <44233030+LehtiniemiJ@users.noreply.github.com>
2019-12-01 21:26:31 +0200

    fixed bug
    asked two times for the key

--- AES_work2.py
+++ AES_work2.py
@@ -23,25 +23,22 @@ def encrypt_file(filepath,iv):
                 fout.write(encd)
 
 def decrypt_file(filepath):
+    key = new_generate_key.key_gen(32) #should be 128,192,256 bits long (respectively for AES-128, AES-192 or AES-256).
     with open (filepath, 'rb+') as f:
         with open ('dec.aes','w') as fout:
-            while True:
-                key = new_generate_key.key_gen(32) #should be 128,192,256 bits long (respectively for AES-128, AES-192 or AES-256).
-                data = f.read()
-                iv = data[:16]
-               # print(iv)
-               # print(len(iv))
-                data = data[16:]
-                n = len(data)
-                aes = AES.new(key, AES.MODE_CBC, iv)
-                if n == 0:
-                    break
-                else:
-                    #print('length',len(data))
-                   # print(data)
-                    decd = aes.decrypt(data).decode('utf-8')
-                    #print (decd)
-                    fout.write(decd)
+            #while True:
+            data = f.read()
+            iv = data[:16]
+            #print(iv)
+            #print(len(iv))
+            data = data[16:]
+            n = len(data)
+            aes = AES.new(key, AES.MODE_CBC, iv)
+            #print('length',len(data))
+            # print(data)
+            decd = aes.decrypt(data).decode('utf-8')
+            #print (decd)
+            fout.write(decd)
 
 
 #iv = os.urandom(16)  #generate iv
